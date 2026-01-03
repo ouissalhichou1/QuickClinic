1ca8ea1859baa6e9e1620d133ea63983416cebdb
OUISSAL EL HICHOU <114773271+ouissalhichou1@users.noreply.github.com>
2026-01-03 12:50:02 +0100

    testtttt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     <div className='mx-4 sm:mx-[10%]'>
 <Navbar/>
     <Routes>
-      <Route path='/' element={<Home kkkkk fffff />}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/doctors' element={<Doctors />}/>
       <Route path='/doctors/:speciality' element={<Doctors />}/>
       <Route path='/login' element={<Login />}/>
